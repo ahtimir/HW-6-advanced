491e9c741de7b4f81a8ce527f53567f9d4b265ae
ahtimir <ahtimir@gmail.com>
2020-12-17 22:31:04 +0300

    initial commit

---
+++ src/js/__tests__/destruct.test.js
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+
+import destructuringObject from '../destruct';
+
+test('Reveal special attack', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  expect(destructuringObject(character)).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
---
+++ src/js/__tests__/forin.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable linebreak-style */
+
+import orderByProps from '../forin';
+
+const obj = {
+  name: 'мечник',
+  health: 10,
+  level: 2,
+  attack: 80,
+  defence: 40,
+};
+
+test('Order By Props - Level', () => {
+  expect(orderByProps(obj, ['name', 'level'])).toEqual([
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ]);
+});
+
+test('Order By Props - Attack', () => {
+  expect(orderByProps(obj, ['name', 'attack'])).toEqual([
+    { key: 'attack', value: 80 },
+    { key: 'name', value: 'мечник' },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+    { key: 'level', value: 2 },
+  ]);
+});
+
+test('Order By Props - Health', () => {
+  expect(orderByProps(obj, ['name', 'health'])).toEqual([
+    { key: 'health', value: 10 },
+    { key: 'name', value: 'мечник' },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'level', value: 2 },
+  ]);
+});
